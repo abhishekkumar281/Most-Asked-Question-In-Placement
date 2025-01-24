bc778827fe5d203859008a9dc61451a7b8c18427
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2025-01-24 09:50:24 +0530

    Add files via upload

---
+++ Check_PrimeNumber.java
@@ -0,0 +1,23 @@
+package INTERVIEWQUETION;
+
+public class Check_PrimeNumber {
+    static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < n/2; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+        }
+        public static void main(String args[]){
+            if (isPrime(13))
+                System.out.println(" true");
+            else
+                System.out.println(" false");
+            if (isPrime(20))
+                System.out.println(" true");
+            else
+                System.out.println(" false");
+    }
+}
