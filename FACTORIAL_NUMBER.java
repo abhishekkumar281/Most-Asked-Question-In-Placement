083a4b3bf1f43c45997e95e01b10ae525081f126
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2025-02-04 15:01:38 +0530

    Add files via upload

---
+++ FACTORIAL_NUMBER.java
@@ -0,0 +1,17 @@
+package INTERVIEWQUETION;
+
+class FACTORIAL_NUMBER {
+    static int factorial(int n) {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is "
+                + factorial(5));
+    }
+}
+
